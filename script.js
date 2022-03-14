011d894c2e14b8461e91fec7a9fc95494073eced
Gagan Surya BS <94371888+gagan-surya-bs@users.noreply.github.com>
2022-03-14 08:48:21 +0530

    react task commit

---
+++ script.js
@@ -0,0 +1,73 @@
+// let array = [
+//   {
+//     name: "bhaskar",
+//     formfilter: "false",
+//     x: "jjj",
+//   },
+//   {
+//     name: "keelu",
+//     formfilter: "true",
+//   },
+//   {
+//     name: "bhaskar",
+//     formfilter: "false",
+//     x: "jjj",
+//   },
+// ];
+// let filtered = array.filter(value => {
+//   if (value.name === "keelu") {
+//     return (value.formfilter = "false");
+//   } else if (value.name === "bhaskar") {
+//     return (value.formfilter = "true");
+//   } else {
+//     return value;
+//   }
+// });
+// console.log(filtered);
+
+//  row = 10;
+//  col = 10;
+
+//  let str = "";
+//  for (let i = 1; i < row; i++) {
+//    let str = "";
+//    for (let j = 0; j < col; j++) {}
+//    console.log(str);
+//  }
+
+// let arr = [5, 8, 3, 4];
+
+// let temp;
+
+// for (let i = 0; i < arr.length; i++) {
+//   for (let j = 1; j < arr.length; j++) {
+//     if (arr.length % 2 === 0) {
+//       temp = arr[j];
+//       arr[j] = arr[i];
+//       arr[i] = temp;
+//     }
+//   }
+// }
+
+// console.log(arr);
+
+//let arr = "aaabbccddd";
+
+// for (let i = 0; i < arr.length; i++) {
+//   let count = 0;
+//   for (let j = 0; j < arr.length; j++) {
+//     if (arr[i] === arr[j]) {
+//       count++;
+//     }
+//   }
+//   console.log(`${arr[i]} : ${count}  `);
+// }
+
+let name = students.for((value, index) => {
+  if (value.age === "12") {
+   console.log(value.name)
+  }
+})
+console.log(name)
+
+
